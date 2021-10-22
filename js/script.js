5833ef9180a69d6dd4a8cba5b5a0ea86c6f82aef
William Durocher <durocherwilliam@gmail.com>
2021-10-21 20:42:46 -0400

    Added first vers. of the end of game screen

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 /**
  * TODO:
- * -Add end of game screen + try again Button
  * -Add surprise
  * -Add info screen
  * -Add effect when first opening app (?)
@@ -23,10 +22,7 @@ function playRound(playerSelection, computerSelection) {
 
     if (computerSelection === playerSelection) {
         result = `Round is null, both players chose ${computerSelection}`;
-        flashLabelsColor('yellow');
-
-
-        
+        flashLabelsColor('yellow');      
     }
     else if (
         (computerSelection === choices[0] && playerSelection === choices[2]) ||
@@ -35,16 +31,11 @@ function playRound(playerSelection, computerSelection) {
         result = `You lose! ${computerSelection} beats ${playerSelection}!`;
         updateScore(COMPUTER_SCORE_ID);
         flashLabelsColor('red');
-
-
-
     } else {
         result = `You win! ${playerSelection} beats ${computerSelection}!`;
         updateScore(PLAYER_SCORE_ID);
         flashLabelsColor('green');
-
     }
-
     return result;
 }
 
@@ -75,7 +66,7 @@ function flashLabelsColor(color){
         cpu.style.color = "white"; 
         player.style.color = "white"; 
 
-    }, 500);
+    }, 800);
 }
 
 //play 5 games against computer
@@ -88,17 +79,51 @@ function game() {
     let playerSelection = prompt("Rock, Paper, Scissors?");
     console.log(playRound(playerSelection, computerPlay()));
 }
-const buttons = document.querySelectorAll('.btn');
+const buttons = document.getElementById('btn-container').querySelectorAll('.btn');
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         //alert(
         console.log(playRound(button.id, computerPlay()));
-        if (getScore(COMPUTER_SCORE_ID) == 5 || getScore(PLAYER_SCORE_ID) == 5) {
-            alert('winner winner chicken dinner');
+        if (getScore(COMPUTER_SCORE_ID) == 5){
+            showEndGame(COMPUTER_SCORE_ID);
+        }else if(getScore(PLAYER_SCORE_ID) == 5) {
+            showEndGame(PLAYER_SCORE_ID);
             resetScores();
         }
     });
 });
 
+const tryAgainbtn = document.getElementById('endgame-btn');
 
+tryAgainbtn.addEventListener('click', () =>{
+    restartGame();
+})
+
+function showEndGame(){
+hideGame();
+document.getElementById('endgame').innerText = "You lost the game dawg";
+document.getElementById('endgame-btn').innerText = "try again";
+
+
+
+}
+
+function hideGame(){
+    document.getElementById('description-container').style.visibility = 'hidden';
+    document.getElementById('btn-container').style.visibility = 'hidden';
+    document.getElementById('score-container').style.visibility = 'hidden';
+    document.getElementById('score-label').style.visibility = 'hidden';
+    document.getElementById('endgame-container').style.display = 'flex';
+    
+}
+
+function restartGame(){
+    document.getElementById('description-container').style.visibility = 'visible';
+    document.getElementById('btn-container').style.visibility = 'visible';
+    document.getElementById('score-container').style.visibility = 'visible';
+    document.getElementById('score-label').style.visibility = 'visible';
+    document.getElementById('endgame-container').style.display = 'none';
+
+    resetScores();
+}
